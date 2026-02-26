d5d9d8a1bf1ccd8f5f12c79205511ed024f3aebf
jvilleda <jesus.vill.25@gmail.com>
2026-02-26 10:10:31 -0600

    [JVR] Creación del archivo analisis.py

---
+++ analisis.py
@@ -0,0 +1,11 @@
+# Autor: JVR (Jesús Villeda Ramírez)
+# Fecha: 26-02-2026
+# Explicación: Script inicial para la práctica de ingeniería de software.
+
+
+def main():
+    print("Hola equipo 4. Este es el script de analisis.py cargado exitosamente.")
+
+
+if __name__ == "__main__":
+    main()
